72958ee516388e7d3a41f0d8d974f593e623c999
LavanyaBS2022 <lavanyabs2022@gmail.com>
2024-07-29 10:56:22 +0530

    APK creation completed with appicon and splash screen

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './Screens/LoginScreen';
 import ListScreen from './Screens/ListScreen';
+import SplashScreen from 'react-native-splash-screen'
+import { Platform } from 'react-native';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  React.useEffect(()=>{
+    if(Platform.OS==='android')SplashScreen.hide();
+  },[])
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
